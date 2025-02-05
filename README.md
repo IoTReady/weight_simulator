fa4d95e0e99fc12caacc112a8b661c83d54d4a8e
Tej Pochiraju <tej@iotready.co>
2025-02-05 13:43:00 +0530

    Change format to Toledo

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 `file_gen.py` - Generate weights and save on new lines to a file.
 `file_parse.py` - Parse weights from a file.
 `serial_gen.py` - Generate weights and send via serial to simulate a weighing scale.
+
+
+# Usage:
+
+`python3 serial_gen.py /dev/ttyUSB0`
--- serial_gen.py
+++ serial_gen.py
@@ -4,15 +4,17 @@ from random import randint
 
 SAMPLES_PER_SECOND = 10
 
-with open(sys.argv[1], 'w') as f:
+with open(sys.argv[1], "w") as f:
     while True:
         ok = True
-        if randint(1,10) > 8:
+        if randint(1, 10) > 8:
             ok = False
         if ok:
-            weight = randint(18000,18020)
+            weight = randint(2400, 2800)
         else:
-            weight = randint(-1500,20000)
-        print(weight)
-        f.write(str(weight)+'\r\n')
-        sleep(1/SAMPLES_PER_SECOND)
+            weight = randint(-1500, 2000)
+        s = f"W *0      {weight}      0\r\n"
+        print(s)
+        # f.write(str(weight) + "\r\n")
+        f.write(s)
+        sleep(1 / SAMPLES_PER_SECOND)
